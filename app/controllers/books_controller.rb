5a36f43830345e8316d4b8c917575236f7fe95ff
BA10432CB <129589876+BA10432CB@users.noreply.github.com>
2023-05-05 00:15:00 +0000

    [Rails]課題2制作途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,16 +2,33 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to list_path(book.id)
   end
-  
+
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+    book.save
+    redirect_to list_path(book.id)
+  end
+
+  def update
+
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'books/new'
-  post 'books' => 'books/create'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  post 'books' => 'books#create'
+  get 'books' =>'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit'
+  put 'books' => 'books#update'
   get 'homes/top'
 end
